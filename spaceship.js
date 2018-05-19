7562abaa1447614e9c28ab6788e79c5de7b12d31
Berry  Semexan <bsemexan@gmail.com>
2018-05-19 20:42:08 +0000

    automatically backed up by learn

--- spaceship.js
+++ spaceship.js
@@ -1,5 +1,10 @@
 class Spaceship {
   constructor(name, phasers, shields, cloaked, warpDrive, docked, phasersCharge) {
     this.name = name
+    this.phasers = phasers
+    this.shields = cloaked
+    this.warpDrive = warpDrive
+    this.docked = docked
+    this.phasersCharge = phasersCharge 
   }
 }
